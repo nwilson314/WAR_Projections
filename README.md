de07785b2b06114041e97b64b2f90687ca5937e4
Nate Wilson <nsw.wilson@gmail.com>
2019-11-24 15:38:55 -0500

    created current year output of WAR prediction using only current year stats

--- README.md
+++ README.md
@@ -6,9 +6,15 @@
 - Create output of current year WAR prediction based on stats
 - Create output of following year WAR based on stats
 - Multi-year output of WAR
+- Generalized data/ML functions?
 
 ## Dependencies
 - pybaseball: https://github.com/jldbc/pybaseball
 - Pandas
 - Scikit Learn
 - Matplotlib
+
+## Resources
+The following were helpful in varying degrees when working on the model:
+
+- https://www.datacamp.com/community/tutorials/scikit-learn-tutorial-baseball-1
--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pandas as pd
 import sqlite3
 from database import Database
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.linear_model import RidgeCV
 
 DB_NAME = 'database.sqlite'
 
-def get_dataframe(conn):
+def get_dataframe(conn, cols):
 	c = conn.cursor()
 
 	c.execute("SELECT * FROM batting")
@@ -13,12 +16,6 @@ def get_dataframe(conn):
 	rows = c.fetchall()
 
 	players_df = pd.DataFrame(rows)
-	cols = ['Season', 'Name', 'Team', 'Age', 'G', 'AB', 
-				'PA', 'H', '1B', '2B', '3B', 'HR', 
-				'RBI', 'SB', 'AVG',
-				'BB%', 'K%', 'BB/K', 'OBP', 'SLG', 'OPS',
-				'ISO', 'BABIP', 'GB/FB', 'LD%', 'GB%', 
-				'FB%', 'IFFB%', 'wOBA', 'WAR', 'wRC+']
 
 	players_df.columns = cols
 
@@ -37,7 +34,7 @@ def basic_database_plots(df):
 	plt.title('Dist of WAR')
 	plt.show()
 
-def create_training_test_sets(df):
+def create_train_set_current_year(df, attributes, cols):
 	'''
 	Creates training and test sets using the passed pandas dataframe
 
@@ -46,13 +43,6 @@ def create_training_test_sets(df):
 			set respectively
 	'''
 
-	cols = ['Age', 'G', 'AB', 
-				'PA', 'H', '1B', '2B', '3B', 'HR', 
-				'RBI', 'SB', 'AVG',
-				'BB%', 'K%', 'BB/K', 'OBP', 'SLG', 'OPS',
-				'ISO', 'BABIP', 'GB/FB', 'LD%', 'GB%', 
-				'FB%', 'IFFB%', 'wOBA', 'wRC+']
-
 	data = df[cols]
 
 	train_df = data.sample(frac=0.75, random_state=1)
@@ -60,13 +50,58 @@ def create_training_test_sets(df):
 
 	return train_df, test_df
 
+
+def current_year_LR(train_df, test_df, attributes):
+	x_train = train_df[attributes]
+	y_train = train_df['WAR']
+
+	x_test = test_df[attributes]
+	y_test = test_df['WAR']
+
+	lr = LinearRegression(normalize=True)
+	lr.fit(x_train, y_train)
+	predictions = lr.predict(x_test)
+
+	# Determine mean absolute error
+	mae = mean_absolute_error(y_test, predictions)
+
+	# Print `mae`
+	print(mae)
+
+	rrm = RidgeCV(alphas=(0.0001, 0.001, 0.01, 0.1), normalize=True)
+	rrm.fit(x_train, y_train)
+	predictions_rrm = rrm.predict(x_test)
+
+	# Determine mean absolute error
+	mae_rrm = mean_absolute_error(y_test, predictions_rrm)
+	print(mae_rrm)
+
+
+
 def main():
 	db = Database()
 	conn = db.create_connection(DB_NAME)
+
+	# List of column labels we are using from the database
+	cols = ['Season', 'Name', 'Team', 'Age', 'G', 'AB', 
+				'PA', 'H', '1B', '2B', '3B', 'HR', 
+				'RBI', 'SB', 'AVG',
+				'BB%', 'K%', 'BB/K', 'OBP', 'SLG', 'OPS',
+				'ISO', 'BABIP', 'GB/FB', 'LD%', 'GB%', 
+				'FB%', 'IFFB%', 'wOBA', 'WAR', 'wRC+']
 	
-	players_df = get_dataframe(conn)
-	# Next line prints the number of NULL rows per column
+	players_df = get_dataframe(conn, cols)
+
+	# Next line prints the number of NULL rows per column:
 	# print(players_df.isnull().sum(axis=0).tolist())
+
+	# List of attributes we care about from the dataset
+	attributes = ['Age', 'G', 'AB', 
+				'PA', 'H', '1B', '2B', '3B', 'HR', 
+				'RBI', 'SB', 'AVG',
+				'BB%', 'K%', 'BB/K', 'OBP', 'SLG', 'OPS',
+				'ISO', 'BABIP', 'GB/FB', 'LD%', 'GB%', 
+				'FB%', 'IFFB%', 'wOBA', 'wRC+']
 
 	# Turn all NULL values into the median of the non-NULL (might not be smart)
 	players_df['GB/FB'] = players_df['GB/FB'].fillna(players_df['GB/FB'].median())
@@ -76,10 +111,13 @@ def main():
 	players_df['IFFB%'] = players_df['IFFB%'].fillna(players_df['IFFB%'].median())
 
 	# Use the below command to print out some basic plots to visualize the 
-	# database
+	# database:
 	# basic_database_plot(players_df)
 
-	train_df, test_df = create_training_test_sets(players_df)
+	# Use the below for predictions of the current year WAR using only the stats
+	# from that year:
+	# train_df, test_df = create_train_set_current_year(players_df, attributes, cols)
+	# current_year_LR(train_df, test_df, attributes)
 
 	conn.close()
 
